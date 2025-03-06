732c8a4f642489b946227311bb39562bbd48f4b3
unknown <sachinchakradhar8@gmail.com>
2025-03-06 18:28:40 +0545

    deploy

---
+++ app/page.jsx
@@ -0,0 +1,93 @@
+"use client";
+
+import Container from "../components/landing_page/Container";
+import Footer from "../components/landing_page/Footer";
+import Header from "../components/landing_page/Header";
+import Navbar from "../components/landing_page/Navbar";
+import SecondContainer from "../components/landing_page/SecondContainer";
+import Videocontainer from "../components/landing_page/VideoContainer";
+import '@grapesjs/studio-sdk/style';
+import { useState } from "react";
+
+import { Editor } from 'grapesjs';
+import GrapesJsStudio, {
+  StudioCommands,
+  ToastVariant,
+} from '@grapesjs/studio-sdk/react';
+
+
+
+export default function Home() {
+
+  const [editor, setEditor] = useState<Editor>();
+
+  const onReady = (editor) => {
+    console.log('Editor loaded', editor);
+    setEditor(editor);
+  };
+
+  const showToast = (id) =>
+    editor?.runCommand(StudioCommands.toastAdd, {
+      id,
+      header: 'Toast header',
+      content: 'Data logged in console',
+      variant: ToastVariant.Info,
+    });
+
+  const getProjetData = () => {
+    if (editor) {
+      console.log({ projectData: editor?.getProjectData() });
+      showToast('log-project-data');
+    }
+  };
+
+  const getExportData = () => {
+    if (editor) {
+      console.log({ html: editor?.getHtml(), css: editor?.getCss() });
+      showToast('log-html-css');
+    }
+  };
+
+  return (
+    <>
+      
+      <Navbar />
+      <Header />
+      <main className="flex h-screen flex-col justify-between p-5 gap-2">
+      <div className="p-1 flex gap-5">
+        <div className="font-bold">SDK example Next.js</div>
+        <button className="border rounded px-2" onClick={getProjetData}>
+          Log Project Data
+        </button>
+        <button className="border rounded px-2" onClick={getExportData}>
+          Log HTML/CSS
+        </button>
+      </div>
+      <div className="flex-1 w-full h-full overflow-hidden">
+        <GrapesJsStudio
+          onReady={onReady}
+          options={{
+            licenseKey: '9288b413eb9f40728a30256f2502f00260929631643e4d7b8eac2c36bf132450',
+            project: {
+              default: {
+                pages: [
+                  {
+                    name: 'Home',
+                    component: `<h1 style="padding: 2rem; text-align: center">
+                      Hello Studio 👋
+                    </h1>`,
+                  },
+                ],
+              },
+            },
+          }}
+        />
+      </div>
+    </main>
+      <Videocontainer />
+      <Container />
+      <SecondContainer />
+      <Footer />
+  </>
+  );
+}
